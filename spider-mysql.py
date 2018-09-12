da079f4fa641e6e0d5d093fd73a06a7034850651
imorta <evanxian9@gmail.com>
2018-09-12 18:32:20 +0800

    Update spider-mysql.py

--- spider-mysql.py
+++ spider-mysql.py
@@ -3,7 +3,7 @@ import time
 import json
 import pymysql
 from config import *
-from requests.exceptions import ConnectionError, ReadTimeout, ConnectTimeout
+from requests.exceptions import ConnectionError, ReadTimeout, ConnectTimeout, ChunkedEncodingError
 from multiprocessing import Pool
 
 
@@ -29,15 +29,18 @@ def get_space(mid):
         req = requests.get(url, headers=headers, timeout=100)
         if req.status_code == 200:
             print('bili用户主页url:{}'.format(url))
-            print('成功进入用户主页')
             # 获取用户个人信息
             get_GetINnfo(mid)
         else:
             print('进入bili用户主页失败,code {}'.format(req.status_code))
-    except (ConnectionError, ReadTimeout, ConnectTimeout) as e:
-        print(e)
+    except (ConnectionError, ConnectTimeout) as e: 
+        print('网络异常', e)
         time.sleep(5)
-        print('网络异常')
+    except ReadTimeout as e:
+        print('达到递归深度', e)
+        time.sleep(5)
+    except ChunkedEncodingError as e:
+        print('ChunkedEncodingError', e)
 
 
 def get_GetINnfo(mid):
@@ -56,10 +59,8 @@ def get_GetINnfo(mid):
             'mid': str(mid)
         }
         url = 'https://space.bilibili.com/ajax/member/GetInfo'
-
         req = requests.post(url, headers=headers, data=data, timeout=100)
         if req.status_code == 200:
-            print('获取用户个人信息成功')
             status = req.json()
             if status.get('data'):
                 data = status.get('data')
@@ -73,15 +74,17 @@ def get_GetINnfo(mid):
                     'birthday': data.get('birthday'),
                     'sign': data.get('sign')
                 }
-                print('用户个人信息:{}'.format(result))
+                #print('用户个人信息:{}'.format(result))
                 save_GetINnfo_mysql(result)
         else:
             print('获取用户个人信息失败,code {}'.format(req.status_code))
 
-    except (ConnectionError, ReadTimeout, ConnectTimeout) as e:
-        print(e)
+    except (ConnectionError, ConnectTimeout) as e: 
+        print('网络异常', e)
         time.sleep(5)
-        print('网络异常')
+    except ReadTimeout as e:
+        print('达到递归深度', e)
+        time.sleep(5)
 
 
 def get_myinfo(mid):
@@ -98,9 +101,9 @@ def get_myinfo(mid):
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
         }
         url = 'https://api.bilibili.com/x/space/myinfo?jsonp=jsonp'
-        print('获取用户关注数量和粉丝数量url:{}'.format(url))
         req = requests.get(url, headers=headers, timeout=100)
         if req.status_code == 200:
+            print('获取用户关注数量和粉丝数量url:{}'.format(url))
             status = req.json()
             if status.get('data'):
                 data = status.get('data')
@@ -113,10 +116,12 @@ def get_myinfo(mid):
 
         else:
             print('get_myinfo url失败 code:{}'.format(req.status_code))
-    except (ConnectionError, ReadTimeout, ConnectTimeout) as e:
-        print(e)
+    except (ConnectionError, ConnectTimeout) as e: 
+        print('网络异常', e)
         time.sleep(5)
-        print('网络异常')
+    except ReadTimeout as e:
+        print('达到递归深度', e)
+        time.sleep(5)
 
 
 def get_followings(mid, pn, ps):
@@ -136,9 +141,9 @@ def get_followings(mid, pn, ps):
         url = 'https://api.bilibili.com/x/relation/followings?vmid=' + \
             str(mid) + '&pn=' + str(pn) + '&ps=' + \
             str(ps) + '&order=desc&jsonp=jsonp'
-        print('获取关注用户信息url:{}'.format(url))
         req = requests.get(url, headers=headers, timeout=100)
         if req.status_code == 200:
+            print('获取关注用户信息url:{}'.format(url))
             code = req.json()
             if code.get('data'):
                 glist = code.get('data').get('list')
@@ -147,7 +152,7 @@ def get_followings(mid, pn, ps):
                         'uname': i.get('uname'),
                         'mid': i.get('mid')
                     }
-                    print(result)
+                    #print(result)
                     # 得到mid进入用户主页面
                     get_space(result.get('mid'))
                     # 保存关注用户的mid到数据库
@@ -158,10 +163,12 @@ def get_followings(mid, pn, ps):
         else:
             print('获取关注用户信息失败 code:{}'.format(req.status_code))
 
-    except (ConnectionError, ReadTimeout, ConnectTimeout) as e:
-        print(e)
+    except (ConnectionError, ConnectTimeout) as e: 
+        print('网络异常', e)
         time.sleep(5)
-        print('网络异常')
+    except ReadTimeout as e:
+        print('达到递归深度', e)
+        time.sleep(5)
 
 
 def get_followers(mid, pn, ps):
@@ -182,9 +189,9 @@ def get_followers(mid, pn, ps):
         url = 'https://api.bilibili.com/x/relation/followers?vmid=' + \
             str(mid) + '&pn=' + str(pn) + '&ps=' + \
             str(ps) + '&order=desc&jsonp=jsonp'
-        print('获取粉丝用户信息url:{}'.format(url))
         req = requests.get(url, headers=headers, timeout=100)
         if req.status_code == 200:
+            print('获取粉丝用户信息url:{}'.format(url))
             code = req.json()
             if code.get('data'):
                 glist = code.get('data').get('list')
@@ -193,7 +200,7 @@ def get_followers(mid, pn, ps):
                         'uname': i.get('uname'),
                         'mid': i.get('mid')
                     }
-                    print(result)
+                    #print(result)
                     # 得到mid进入用户主页面
                     get_space(result.get('mid'))
                     # 保存粉丝用户mid到数据库
@@ -203,10 +210,12 @@ def get_followers(mid, pn, ps):
 
         else:
             print('获取所有粉丝用户信息失败 code:{}'.format(req.status_code))
-    except (ConnectionError, ReadTimeout, ConnectTimeout) as e:
-        print(e)
+    except (ConnectionError, ConnectTimeout) as e: 
+        print('网络异常', e)
         time.sleep(5)
-        print('网络异常')
+    except ReadTimeout as e:
+        print('达到递归深度', e)
+        time.sleep(5)
 
 
 def save_followers_mysql(result):
@@ -214,7 +223,7 @@ def save_followers_mysql(result):
     将关注和粉丝mid保存至mysql
     """  
     try:
-        name = result.get('uname')
+        name = result.get('name')
         mid = result.get('mid')
         with db.cursor() as cursor:
             sql = "SELECT `mid` FROM `list` WHERE `mid`='%s'" % (mid)
@@ -222,13 +231,12 @@ def save_followers_mysql(result):
             r = cursor.fetchone()
             if r:
                 for i in r:
-                    ir = i
-                if ir == mid:
-                    print('数据库已存在该mid {}'.format(r))
+                    if int(i) == int(mid):
+                        print('数据库已存在该mid {}'.format(mid))
             else:
-                sql = """INSERT INTO `list` (`mid`, `name`) VALUES (%d, "%s")""" % (mid, pymysql.escape_string(name))
+                sql = """INSERT INTO `list` (`mid`, `name`) VALUES ("%s", "%s")""" % (mid, pymysql.escape_string(name))
                 cursor.execute(sql)
-                print('{} 关注粉丝保存到数据库成功'.format(result))
+                print('{} 关注粉丝保存到数据库成功'.format(mid))
     finally:
         db.commit()
 
@@ -250,14 +258,13 @@ def save_GetINnfo_mysql(result):
             r = cursor.fetchone()
             if r:
                 for i in r:
-                    ir = i
-                if ir == mid:
-                    print('数据库已存在该用户 {}'.format(r))
+                    if int(i) == int(mid):
+                        print('数据库已存在该用户 {}'.format(mid))
             else:
-                sql = """INSERT INTO `myinfo` (`mid`, `name`, `sex`, `regtime`, `birthday`, `sign`) VALUES (%d, "%s","%s", "%s","%s", "%s")""" % (
+                sql = """INSERT INTO `myinfo` (`mid`, `name`, `sex`, `regtime`, `birthday`, `sign`) VALUES ("%s", "%s","%s", "%s","%s", "%s")""" % (
                     mid, pymysql.escape_string(name), sex, regtime, birthday, pymysql.escape_string(sign))
                 cursor.execute(sql)
-                print('{} 用户信息保存到数据库成功'.format(result))
+                print('{} 用户信息保存到数据库成功'.format(mid))
     finally:
         db.commit()
 
@@ -274,25 +281,24 @@ def run(mid):
         f, g = get_myinfo(mid)
 
         # 获取关注用户信息
-        f_g_ps = 50
-        f_g_pn = int(g / f_g_ps)+1
-        if f_g_pn <= 1:
-            get_followers(mid, 1, f_g_ps)
+        sum_wing = int(g / 50)+1
+        if sum_wing <= 1:
+            get_followings(mid, 1, 50)
         else:
-            for g_pn in range(1, f_g_pn):
-                get_followings(mid, g_pn, f_g_ps)
+            for i in range(1, sum_wing):
+                get_followings(mid, i, 50)
 
         # 获取粉丝用户信息
-        f_r_ps = 50
-        f_r_pn = int(f / f_r_ps)+1
-        if f_r_pn <= 1:
-            get_followers(mid, 1, f_r_ps)
+        sum_wer = int(f / 50)+1
+        if sum_wer <= 1:
+            get_followers(mid, 1, 50)
         else:
-            for r_pn in range(1, f_r_pn):
-                get_followers(mid, r_pn, f_r_ps)
+            for i in range(1, sum_wer):
+                get_followers(mid, i, 50)
 
         # 提取一个mid继续运行
         return run(rep_run())
+
     except ReadTimeout as e:
         print('达到递归深度', e)
         time.sleep(5)
@@ -321,24 +327,19 @@ def rep_run():
                 db.close()
                 exit()
             else:
-                sql = "SELECT `mid` FROM `list` WHERE id=%d" % (MIN)
+                sql = "SELECT `mid` FROM `list` WHERE id='%s'" % (MIN)
                 cursor.execute(sql)
                 r = cursor.fetchone()
                 if r:
                     for i in r:
                         return i
+
     except ReadTimeout as e:
         print('达到递归深度', e)
         time.sleep(5)
 
 
 if __name__ == '__main__':
-    # 最好填写自己的mid
-    start = time.time()
-    MMID = rep_run()
+
     with Pool(10) as p:
         p.map(run, [USERMID])
-    end = time.time()
-    print(end - start)
-    
-    
